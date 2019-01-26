969c7b8094587825aca006bc68e590b6fbaa515a
Peter Tucker <ptucker@whitworth.edu>
2019-01-26 14:29:25 -0800

    feedback

--- Exam/src/Main.java
+++ Exam/src/Main.java
@@ -1,3 +1,20 @@
+/*
+FEEDBACK
+
+Read from the file correctly: 20/20%
+Implement class hierarchy correctly: 20/20%
+Populate appropriate data structures with shape data: 15/15%
+Show list of shapes correctly: 15/15%
+Show specific shape properties correctly: 15/15%
+Well-structured, well-commented code: 13/15%
+
+
+Comments:
+  Shape doesn't need to implement toString, since that's on Object
+  Shape shouldn't implement the constructor. Make the sub classes implement that.
+
+*/
+
 /**
  * Project: Exam
  * Package: PACKAGE_NAME
